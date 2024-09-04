bd274869c89f7ceddeb48d0d6a5b619b333d3eb4
Golenastov Aleksandr <96904526+Emp-tyname@users.noreply.github.com>
2024-09-04 23:36:08 +0300

    Add files via upload

--- task4/Task_4.py
+++ task4/Task_4.py
@@ -1,6 +1,15 @@
-nums = input('Пожалуйста, введите имя файла, содержащего массив целых чисел\n')
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('input_data', type=str, help='Число n')
+obj = parser.parse_args()
+obj_dict = vars(obj)
+nums = obj_dict['input_data']
+
+
 with open(nums, 'r') as file:
     numbers = [int(i) for i in file.read().strip().split('\n')]
-median = sum(numbers) // len(numbers)  # Поиск медианы для ряда
+print((sorted(numbers)),  len(numbers))
+median = sorted(numbers)[len(numbers) // 2] # Рассчет медианы
+print(median)
 moves_number = sum([abs(median - i) for i in numbers])  # Сумма разностей по модулю медианы и каждого значения
 print(moves_number)
